3929f8ec0c1dffc608066640d4c724d411fddce5
vizvarun <varunvishal10@gmail.com>
2020-06-18 00:44:07 +0530

    firebase added

--- App.js
+++ App.js
@@ -8,73 +8,27 @@ import SettingsPage from "./screens/Settings";
 import PageA from "./screens/PageA";
 import PageB from "./screens/PageB";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { StatusBar } from "react-native";
+import { StatusBar, View } from "react-native";
+import * as firebase from "firebase";
 
-const HomeStack = createStackNavigator();
-
-function HomeStackScreen() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={Home} />
-      <HomeStack.Screen name="PageA" component={PageA} />
-    </HomeStack.Navigator>
-  );
-}
-
-const SettingsStack = createStackNavigator();
-
-function SettingsStackScreen() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Settings" component={SettingsPage} />
-      <SettingsStack.Screen name="PageB" component={PageB} />
-    </SettingsStack.Navigator>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
-
-function TabNavigator() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === "Home") {
-            iconName = focused ? "home" : "home";
-          } else if (route.name === "Settings") {
-            iconName = focused ? "settings" : "settings-outline";
-          }
-
-    
-          return (
-            <MaterialCommunityIcons name={iconName} size={24} color={color} />
-          );
-        },
-      })}
-      tabBarOptions={{
-        showLabel: false,
-        activeTintColor: "black",
-        inactiveTintColor: "gray",
-        activeBackgroundColor: "#f4f4f4",
-      }}
-    >
-      <Tab.Screen name="Home" component={HomeStackScreen} />
-      <Tab.Screen name="Settings" component={SettingsStackScreen} />
-    </Tab.Navigator>
-  );
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyA24RLscEW3GsdcmZ0I0SrzSEuP5fc_0qY",
+  authDomain: "harmony-music-app.firebaseapp.com",
+  databaseURL: "https://harmony-music-app.firebaseio.com",
+  projectId: "harmony-music-app",
+  storageBucket: "harmony-music-app.appspot.com",
+  messagingSenderId: "403211028232",
+  appId: "1:403211028232:web:14183d3fa2e37304da4ebd",
+  measurementId: "G-DDLJVJ69J9",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" backgroundColor="#f4f4f4" />
-      <Drawer.Navigator>
-        <Drawer.Screen name="MenuTab" component={TabNavigator} />
-        <Drawer.Screen name="Home" component={HomeStackScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>Hello World</Text>
+    </View>
   );
 }
