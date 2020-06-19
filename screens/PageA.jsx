bd5e01163b61027acb12f360b74f6836ff8acd35
Varun Vishal <varunvishal10@gmail.com>
2020-06-20 01:36:18 +0530

    Update PageA.jsx

--- screens/PageA.jsx
+++ screens/PageA.jsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#000",
-    width: "95%",
+    width: "100%",
     height: "100%",
   },
   second: {
