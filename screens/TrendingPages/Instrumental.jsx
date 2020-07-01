cd8e2a7f3371b43e53734f196c45d12298e883b4
Satish Singh <singhsatish7275@gmail.com>
2020-07-01 22:29:58 +0530

    Trending page update

---
+++ screens/TrendingPages/Instrumental.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Instrumental() {
+    return (
+        <View>
+            <Text>Instrumental</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
