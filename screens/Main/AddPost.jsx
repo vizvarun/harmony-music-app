39e5428bfba18434b315449fdfbb78b4321614af
Satish Singh <singhsatish7275@gmail.com>
2020-07-03 03:15:30 +0530

    image picker added

--- screens/Main/AddPost.jsx
+++ screens/Main/AddPost.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import TitleBlock from "../../components/TitleBlock";
+import ImagePickerExample from "../../components/ImagePicker";
 
 export default function AddPost() {
   return (
     <>
       <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
         <TitleBlock heroText="Add Post" subText="This is the add post page" />
-        <View style={styles.mainBlock}></View>
+        <View style={styles.mainBlock}>
+        <ImagePickerExample />
+        </View>
       </ScrollView>
     </>
   );
